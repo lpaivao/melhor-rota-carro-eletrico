3ec92757bd4fe79f72d8133060898caa5b4a462d
lucas <lpaivauefs@gmail.com>
2023-04-09 02:08:57 -0300

    Atualizando vaga e recarregando carro

--- posto/posto.py
+++ posto/posto.py
@@ -1,13 +1,15 @@
+import random
 import paho.mqtt.client as client
 import json, schedule, threading,time
 
 class Posto:
-    def __init__(self,ID_POSTO=0,lat=0,lgn=0,ID_NEVOA=1,BROKER_HOST="localhost",BROKER_PORT=1883 ):
+    def __init__(self,ID_POSTO=0,lat=-23.5440,lgn=-46.6340,ID_NEVOA=1,BROKER_HOST="localhost",BROKER_PORT=1883, limite_vagas=5):
         self.ID_POSTO = ID_POSTO
         self.ID_NEVOA = ID_NEVOA
         self.latitude = lat
         self.longitude = lgn
         self.fila = 0
+        self.limite_vagas = limite_vagas
 
         self.BROKER_HOST = BROKER_HOST
         self.BROKER_PORT = BROKER_PORT
@@ -34,38 +36,63 @@ class Posto:
             time.sleep(1)
 
     def on_connect(self,client,usardata,flags,rc):
+        print("Conectado")
        # self.client.subscribe(self.__BETTER_STATION)
         self.client.subscribe(self.__INCRISE_LINE)
         self.client.publish(self.__STATUS,json.dumps({
             "id_posto" : self.ID_POSTO,
-            "latitude" : self.latitude,
-            "longitude" : self.longitude,
             "fila" : self.fila
-
         }))
 
     def on_message(self,client,data,message):
         topic = message.topic.split("/")
-        msg = message.payload.decode()
+        msg = message.payload.decode('utf-8')
+        msg = json.loads(msg)
+        print(message.topic)
 
         if topic[2] == "incrise_line":
-            self.fila += 1
-            print(self.fila)
-            self.client.publish(self.__ALOC,json.dumps({
-                "id_posto" : self.ID_POSTO,
-                "vaga" : True 
-            }))
+            print(f"Entrada na fila do carro: {msg['id_carro']}")
+            if self.fila < self.limite_vagas:
+                self.fila += 1
+                if self.fila == self.limite_vagas:
+                    self.client.publish(self.__ALOC,json.dumps({
+                        "id_posto" : self.ID_POSTO,
+                        "vaga" : False
+                    }))
+                else:
+                    self.client.publish(self.__ALOC,json.dumps({
+                        "id_posto" : self.ID_POSTO,
+                        "vaga" : True
+                    }))
+                id_carro = str(msg["id_carro"])
+                thread = threading.Thread(target=self.recarregar_bateria, args=id_carro)
+                thread.start()
+            else:
+                print(self.fila)
+                self.client.publish(self.__ALOC, json.dumps({
+                    "id_posto": self.ID_POSTO,
+                    "vaga": False
+                }))
 
     def publish_status(self):
         print("Estou publicando status")
         self.client.publish(self.__STATUS,json.dumps({
             "id_posto" : self.ID_POSTO,
-            "latitude" : self.latitude,
-            "longitude" : self.longitude,
             "fila" : self.fila
-
         }))
 
+    def recarregar_bateria(self, id_carro):
+        print(f"Bateria do carro {id_carro} recarregada")
+        topic_pub = f"car/battery_recharged/{id_carro}"
+        payload = {
+            "bateria": 100,
+            "latitude": self.latitude,
+            "longitude": self.longitude
+        }
+        print(topic_pub)
+        payload = json.dumps(payload)
+        self.client.publish(topic_pub, payload)
+        self.publish_status()
 
     def subtrair_fila(self):
         if self.fila > 0:
