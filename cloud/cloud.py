ac9bd3f601f8432253d620dd68b6050e00340267
Anésio Neto <anesios98@gmail.com>
2023-04-11 13:13:11 -0300

    Criação de thread de conexão na Cloud

--- cloud/cloud.py
+++ cloud/cloud.py
@@ -1,68 +1,72 @@
 from flask import Flask, request, jsonify
 from flask_mqtt import Mqtt
-
-fogs = {}
-command_queue = {}
-
-"""
-Topics:
-*The default scheme is:
-    fog{id}/car/#
-    fog{id}/station/#
-
-examples:
--fog1/car/low-battery (The publisher is a car)
--fog1/car/posix (The publisher is a car)
--fog1/station/queue (The publisher is a station)
--fog1/station/better-local (Cars are the subscribers)
--fog1/carro/fog-state (Cars are the subscribers)
-"""
-
+import paho.mqtt.client as mqtt
+import random
+import json
+import topics
+import threading
 
 app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = 'localhost'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = '' 
-app.config['MQTT_PASSWORD'] = ''
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
 
-topic = '/flask/mqtt'
 
-mqtt_client = Mqtt(app)
-
-@mqtt_client.on_connect()
-def handle_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print('Connected successfully')
-        mqtt_client.subscribe(topic) 
-    else:
-        print('Bad connection. Code:', rc)
-        
-@mqtt_client.on_message()
-def handle_mqtt_message(client, userdata, message):
-    """
-    topic = message.topic.split('/')
-    payload = message.payload.decode('utf-8')
-    """
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    print('Received message on topic: {topic} with payload: {payload}'.format(**data))
-
-@app.route('/publish', methods=['POST'])
-def publish_message():
-    request_data = request.get_json()
-    publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
-    return jsonify({'code': publish_result[0]})
-
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
     return jsonify({"message": "Hello, World!",
                     "error": False
                     })
 
+
+class Cloud:
+    def __init__(self, host, port, timeout):
+        self.fog_prefix = "fog"
+        self._host = host
+        self._port = port
+        self._fognodes = {}
+        # Colocar uma queue de verdade (se tiver de usar uma queue!). Pode até ser priority(o comando a ser executado mais rapudo é o que tem o carro com menor bateria)
+        self._command_queue = {}
+        self._id = random.randint(1, 10)
+        self._broker_connection = mqtt.Client(str(self._id), False)
+        self._broker_connection.on_message = self._on_node_message
+
+        connection_thread = threading.Thread(
+            target=self._connect_to_brocker, args=[self._broker_connection, self._host, self._port, timeout])
+        connection_thread.start()
+
+    # fog/fog_id/fog_change
+    # fog/fog_id/response_fog_change
+    def _connect_to_brocker(self, client, host, port, timeout):
+        client.connect(host, port, timeout)
+        print('Connected successfully')
+        client.subscribe(
+            f"{self.fog_prefix}/{topics.FOG_CHANGE}")
+        client.subscribe(
+            f"{self.fog_prefix}/{topics.CLOUD_RESPONSE_FOG}")
+        client.loop_forever()
+
+    def _on_node_message(self, station, message):
+        topic = message.topic.split('/')
+        msg = message.payload.decode()
+        msg = json.loads(msg)
+
+        """
+        Uma garantia de segurança
+        """
+        if topic[0] == str(self.fog_prefix):
+            node_id = topic[1]
+            if topic[2] == topics.FOG_CHANGE:
+                id_carro = msg["id_carro"]
+                latitude = msg["latitude"]
+                longitude = msg["longitude"]
+                max_distance_per_charge = msg["max_distance_per_charge"]
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=65136)
-    
+    try:
+        my_cloud = Cloud("localhost", 1883, 60)
+        # api = threading.Thread(target=app.run)
+        # api.start()
+    except KeyboardInterrupt:
+        print("Ending Application...")
+    finally:
+        # api.join()
+        pass
