12c0ec8f342e666dc6c27197c51516085113f9c7
Esquivel <laraesquivelsantos@gmail.com>
2023-04-13 08:51:52 -0300

    Posto config

--- posto/__init__.py
+++ posto/__init__.py
@@ -1,9 +1,13 @@
 from posto import Posto
 
-
+id = int(input("Insira o id do posto"))
+lat = float(input("Insira a latitude do posto"))
+lgn = float(input("Insira a longitude do posto"))
+id_nevoa = int(input("Insira o id da nevoa associada"))
 host = input("Insira o host do broker")
 port = input('Insira a porta do broker')
-posto = Posto(BROKER_HOST=host,BROKER_PORT=port)
+
+posto = Posto(id,lat,lgn,id_nevoa,BROKER_HOST=host,BROKER_PORT=port)
 
 
 
