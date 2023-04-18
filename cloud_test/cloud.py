c43eea3d455a7634f69769c0ccd5c2a8ac98bb4c
lucas <lpaivauefs@gmail.com>
2023-04-18 01:13:32 -0300

    Nuvem mudando névoa do carro

---
+++ cloud_test/cloud.py
@@ -0,0 +1,122 @@
+import json
+
+from flask import Flask, request, jsonify
+import socket
+import threading
+
+from geopy import Point
+import paho.mqtt.client as mqtt
+
+import functions
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello, World!",
+                    "error": False
+                    })
+
+
+nevoas_var = {
+    "0": {"fog_id": 0, "ponto_central": Point(-23.5450, -46.6350)},
+    "1": {"fog_id": 1, "ponto_central": Point(-23.5600, -46.6600)},
+    "2": {"fog_id": 2, "ponto_central": Point(-23.5480, -46.6380)}
+}
+
+
+class Cloud:
+    def __init__(self, host, port, nevoas=nevoas_var):
+        self.nevoas = nevoas
+        # Conexão MQTT
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.connect('localhost', 1883, 60)
+
+        # Conexão sock
+        self.host = host
+        self.port = port
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((self.host, self.port))
+        self.s.listen()
+
+        self.sentinelthread = threading.Thread(
+            target=self._handle_conn, args=[self.s])
+        self.sentinelthread.start()
+
+        self._fognodes = []
+
+        # Colocar uma queue de verdade (se tiver de usar uma queue!). Pode até ser priority(o comando a ser executado mais rápido é o que tem o carro com menor bateria)
+        self._command_queue = {}
+
+        self.client.loop_forever()
+    def on_connect(self, client, userdata, flags, rc):
+        print(f'Conectado ao Broker! Código de retorno {rc}')
+
+    def _handle_conn(self, socket):
+        while True:
+            conn, addr = socket.accept()
+            self._fognodes.append(conn)
+            print(len(self._fognodes))
+            thread = threading.Thread(
+                target=self._handle_fognode, args=[conn, addr])
+            thread.start()
+
+    def _handle_fognode(self, connection, address):
+
+        with connection:
+            print(threading.current_thread().name)
+
+            # Fazer aqui o processo de trocar o carro de nó
+            while True:
+                try:
+                    data = connection.recv(1024)
+                    if not data:
+                        break
+
+                    # Decodifica a mensagem
+                    message = data.decode("utf-8")
+                    message = json.loads(message)
+                    # Desmembra as informações
+                    id_carro = message["id_carro"]
+                    latitude = message["latitude"]
+                    longitude = message["longitude"]
+                    max_distance_per_charge = message["max_distance_per_charge"]
+                    fog_id = message["fog_id"]
+
+                    # Verifica a névoa nova
+                    nova_nevoa_id = functions.calcula_nevoa_proxima(fog_id, latitude, longitude,
+                                                                    max_distance_per_charge, self.nevoas)
+
+                    # Constrói a resposta
+                    response = {
+                        "fog_id": nova_nevoa_id
+                    }
+                    response = json.dumps(response)
+
+                    # Tópico para enviar a mensagem
+                    topic_pub = f"cloud/fog_change/{id_carro}"
+                    # Envia mensagem
+                    print(f"Mudando carro {id_carro} da nevoa {fog_id} para a nevoa {nova_nevoa_id}")
+                    self.client.publish(topic_pub, response)
+
+                except socket.error:
+                    print("Client disconnected")
+                    break
+
+            self._fognodes.remove(connection)
+            connection.close()
+            threading.current_thread().join()
+
+    def _on_node_message(self):
+        pass
+
+
+if __name__ == '__main__':
+    HOST = 'localhost'
+    PORT = 8000
+    print(f"The cloud is listening on: ({HOST}:{PORT})")
+    my_cloud = Cloud(HOST, PORT)
