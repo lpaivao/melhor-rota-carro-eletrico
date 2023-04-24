4d535e749f96fd8669778afd0ef75ab0e3e337b1
Anésio Neto <anesios98@gmail.com>
2023-04-24 13:29:54 -0300

    Changes on DockerFiles

--- cloud/cloud.py
+++ cloud/cloud.py
@@ -1,5 +1,5 @@
 import json
-
+import argparse
 from flask import Flask, jsonify
 import socket
 import threading
@@ -128,6 +128,30 @@ class Cloud:
 
 
 if __name__ == '__main__':
+    """
+    client = docker.from_env()
+    container = client.containers.get('my-container')
+    network = client.networks.get('my-network')
+    container_ip = network.attrs['Containers'][container.id]['IPv4Address']
+    """
+
+    """
+    print(f"The IP address of container {container.name} on network {network.name} is {container_ip}")
+    parser = argparse.ArgumentParser(
+        description='TEC502:PBL2:2023.1 - Sets up the network config of the cloud')
+    parser.add_argument('--ip', help='IP address', required=True)
+    parser.add_argument('--port', metavar='PORT', type=int, default=8000,
+                        help='TCP port (default 8000)', required=True)
+    parser.add_argument(
+        '--mosquitto_ip', help='Mosquitto IP address', required=True)
+    parser.add_argument('--mosquitto_port', metavar='MOSQUITTO_PORT', type=int, default=1883,
+                        help='Mosquitto port (default 1883)', required=True)
+    args = parser.parse_args()
+
+    HOST = args.ip
+    PORT = args.port    
+    
+    """
     HOST = '127.0.0.1'
     PORT = 8000
     print(f"The cloud is listening on: ({HOST}:{PORT})")
