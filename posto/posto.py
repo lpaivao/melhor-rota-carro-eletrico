0444a2631b45ddffc75766feccfd9ed08059b2e2
AnesioSousa <anesios98@gmail.com>
2023-04-26 17:58:31 -0300

    Posto update - Dockerfile update

--- posto/posto.py
+++ posto/posto.py
@@ -121,12 +121,12 @@ class Posto:
 
 if __name__ == '__main__':
 
-    id = int(input("Insira o id do posto"))
-    lat = float(input("Insira a latitude do posto"))
-    lgn = float(input("Insira a longitude do posto"))
-    id_nevoa = int(input("Insira o id da nevoa associada"))
-    host = input("Insira o host do broker")
-    port = int(input('Insira a porta do broker'))
+    id = int(input("Insira o id do posto: "))
+    lat = float(input("Insira a latitude do posto: "))
+    lgn = float(input("Insira a longitude do posto: "))
+    id_nevoa = int(input("Insira o id da nevoa associada: "))
+    host = input("Insira o host do broker: ")
+    port = int(input('Insira a porta do broker: '))
 
     posto = Posto(id,lat,lgn,id_nevoa,BROKER_HOST=host,BROKER_PORT=port)
 
